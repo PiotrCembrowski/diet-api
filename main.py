1fff651313ffdb08935d2d6fd6649741d0c58a41
Piotr Cembrowski <losttape@gmail.com>
2025-03-13 19:53:26 +0100

    db and products table added

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+@app.post("/products")
+def create_product():
+    
+    return {"message": "Product has been created successfully!"}
